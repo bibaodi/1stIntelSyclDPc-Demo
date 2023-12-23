bfd70563f4b5437f29af6c258bd610fce3bcd742
eton <bbaodi@ace-cardio.com>
2023-12-23 11:15:16 +0800

    add some parameters

--- 1stSyclTutorials/05-robustDeviceDiscovery/src/main.cpp
+++ 1stSyclTutorials/05-robustDeviceDiscovery/src/main.cpp
@@ -46,26 +46,41 @@ inline void showAllDev(){
     }
 }
 
-int main(int, char **) {
-  bool usingDaemon=false;
+int main(int argc, char **argv) {
+std::cout<<"parameter will decide using Daemon or Not.\nargc="<<argc<<", argv0="<<argv[0]<<"\n";
+  bool usingDaemon=true;
+  if(argc>1){
+    usingDaemon=false;
+    std::cout<<"argv1="<<argv[1]<<"\n";
+  }
+  if(usingDaemon){
+    printf("will using daemon\n");
+  }else{
+    printf("will no-daemon\n");
+  }
+
   dpl_main();
   showAllDev();
-  
 	discoverDev();
- 
 
   pid_t pid = getpid();
 
   std::cout << "1:当前进程的PID是： " << pid << std::endl;
   int nochdir = 1, noclose = 1;
-  if (usingDaemon && daemon(nochdir, noclose) < 0) {
+
+  if (usingDaemon){
+    int ret= daemon(nochdir, noclose);
+    std::cerr<<ret<<"**********daemon create*******;\n";
+    if(0!=ret) {
     perror("error to daemon...\n");
     return -2;
+    }
   }
 
   pid = getpid();
   std::cout << "2:当前进程的PID是： " << pid << std::endl;
   std::cout << "running after daemon\n";
+  std::cerr << "running after daemon\n";
   //<<Setup host storage>>
   sycl::float4 a = {1.0, 2.0, 3.0, 4.0};
   sycl::float4 b = {4.0, 3.0, 2.0, 1.0};
@@ -78,9 +93,9 @@ int main(int, char **) {
   sycl::queue queue(device_selector);
   std::cout << "Running on "
             << queue.get_device().get_info<sycl::info::device::name>() << "\n";
-
+{
   //<<Setup device storage>>
-  { // start of scope, ensures data copied back to host
+   // start of scope, ensures data copied back to host
     sycl::buffer<sycl::float4, 1> a_sycl(&a, sycl::range<1>(1));
     sycl::buffer<sycl::float4, 1> b_sycl(&b, sycl::range<1>(1));
     sycl::buffer<sycl::float4, 1> c_sycl(&c, sycl::range<1>(1));
@@ -103,6 +118,6 @@ int main(int, char **) {
             << "------------------\n"
             << "= C { " << c.x() << ", " << c.y() << ", " << c.z() << ", "
             << c.w() << " }" << std::endl;
-
+std::cerr << "running finish.(this info should always show no matter daemon or not.)\n";
   return 0;
 }
