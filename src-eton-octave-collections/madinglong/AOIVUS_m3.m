30def6b3a904bd9b88b8f549e107614e6a782192
eton <bbaodi@ace-cardio.com>
2024-01-04 11:39:29 +0800

    support using k-wave envelop & logComp

--- src-eton-octave-collections/madinglong/AOIVUS_m3.m
+++ src-eton-octave-collections/madinglong/AOIVUS_m3.m
@@ -39,6 +39,7 @@ end
 ##return ;
 %%
 usingkwaveEnv=true;
+usingkwaveCompress=true;
 addpath('/home/eton/00-src/30-octaves/k-wave-toolbox-version-1.4/k-Wave:/home/eton/00-src/30-octaves/k-wave-toolbox-version-1.4/k-Wave/examples');
 
 for j = 1:nAline
@@ -50,23 +51,46 @@ for j = 1:nAline
         envLine = abs(envLine0);                     %% 取希尔伯特变换后的模
     endif
     %     data_out_aline(:,j) = 10*log10(a_line_j./max(a_line_j(:)));
-    data_out_aline(:,j) = 20*log10(envLine); #amplitude not power, change 10 to 20;
+    alines_envloped(:,j)=envLine;
+    if usingkwaveCompress
+        compressLine = logCompression(envLine, 3, true);
+    else
+        compressLine = 20*log10(envLine); #amplitude not power, change 10 to 20;
+    endif
+    alines_compressed(:,j) = compressLine;
+    #data_out_aline(:,j) = compressLine;
 endfor
-
-data_out_aline = data_out_aline - max(data_out_aline(:));
+maxPixelVal=max(alines_compressed(:));
+data_out_aline = 255 * alines_compressed / maxPixelVal;
+#data_out_aline = data_out_aline - max(data_out_aline(:));
 #time0 = datetime('now');
+
+rectImg = data_out_aline;
+f1=figure('Name','RECT-01-image()', "Position", [0, 0, 512, 512]);image(rectImg);
+f2=figure('Name','RECT-01-imagesc()');imagesc(rectImg);
+#return;
+
+## scan convert;
 tic;
 printf("beflore polar running.")
 %% 图像显示 %%
 
 #ElapsedFun = @()
-[outputImg] = Polar_text_01(data_out_aline,nPts);
+[outputImg] = Polar_text_01(data_out_aline, nPts);
+f3=figure('Name','SCVVT-01-image()', "Position", [0, 0, 512, 512]);image(outputImg); ## imshow would be fixed, but image() will auto fit window size;--eton@240104.
+#return;
 toc; #time1 = datetime('now'); above function will cost [Elapsed time is 27.3513 seconds.]
 
 #consumedTime=timeit(ElapsedFun);
 #printf("consumed time=:%d\n", consumedTime);
+#imshow(outputImg);##try convert or just specify [min, max]; b2bi=cast(outputImg, "uint8");
+f4=figure('Name','SCVVT-01-imshow');imshow(outputImg,[0,255], 'tight', 'initialmagnification', 'fit');  %% 图像设置
+title("SCVVT-01");
+#caxis([-15, 0]); should be `caxis([0,255]); caxis("auto");` matlab changed to clim("auto");
+caxis("auto");
+#set (gcf, 'Position', [0,0, 1, 1]);
+get(f4, "Position");
+movegui('center');
 
-imshow(outputImg, 'border', 'tight', 'initialmagnification', 'fit');  %% 图像设置
-% caxis([1250,4000]);
-caxis([-15, 0]);
-set (gcf, 'Position', [0,0, 600, 600]);
+
+#end./
--- src-eton-octave-collections/madinglong/main_readAlinesShowImg.m
+++ src-eton-octave-collections/madinglong/main_readAlinesShowImg.m
@@ -1,4 +1,6 @@
 #eton@240103 read alines in multi files then show image;
+close all;clear all;
+
 addpath("/media/eton/hdd931g/42-workspace4debian/10-ExtSrcs/61.231030-DP-CL-MP-HPC/41-sycl-tutorials-eton/src-eton-octave-collections/madinglong");
 process_frame;
 AOIVUS_m3;
